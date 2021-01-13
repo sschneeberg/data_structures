f3248c1bf2cf9c6b136e562b386499231186ec3a
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-13 01:40:44 -0500

    fix syntax errors

--- graph.py
+++ graph.py
@@ -7,14 +7,14 @@ class Graph:
             self.values[i] = []
 
     def add_edge_directed(self, src, dest):
-        if not self.values[node]: raise ValueError("Node does not exist")
-        self.values[node].append(to)
+        if not self.values[src]: raise ValueError("Node does not exist")
+        self.values[src].append(dest)
 
     def add_edge_undirected(self, src, dest):
-        if not self.values[node]: raise ValueError("Node does not exist")
-        if not self.values[to]: raise ValueError("Node to connect does not exist")
-        self.values[node].append(to)
-        self.values[to].append(node)
+        if not self.values[src]: raise ValueError("Node does not exist")
+        if not self.values[dest]: raise ValueError("Node to connect does not exist")
+        self.values[src].append(dest)
+        self.values[dest].append(src)
 
     def add_node(self, value):
         if value in self.values: raise ValueError("Node already in graph")
